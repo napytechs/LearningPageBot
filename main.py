67ab2b35f5746e4191aa64339b2b0fe5fea25f09
napytechs <82341865+napytechs@users.noreply.github.com>
2022-05-02 18:11:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -591,7 +591,7 @@ def channel_text():
    
     channels = json.loads(channels)
     chan = [bot.get_chat(c) for c in channels if c]
-    ch = [c.username for c in chan]
+    ch = ["@"+c.username for c in chan]
     chan = '\n'.join(ch)
     key = [key for key, val in channels.items()]
     key = [bot.get_chat(k) for k in key]
